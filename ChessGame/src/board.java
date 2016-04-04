7f4eec1b074fa8122a2ad3ed56cfa3859be29492
Gustav_Svennas <gustavsvennas@gmail.com>
2016-04-04 11:21:45 +0200

    board

--- ChessGame/src/board.java
+++ ChessGame/src/board.java
@@ -1,12 +1,21 @@
 /**
  * Created by gurra on 4/4/16.
  */
+import 
 
 
+public class Board {
 
+    private JFrame frame;
 
-public class board {
+    public Board() {
+        this.frame = new JFrame("Board");
+        frame.setSize(700,300);
+        frame.setVisible(true);
+    }
 
-
+    public static void main(String[] args) {
+        new Board();
+    }
 
 }
